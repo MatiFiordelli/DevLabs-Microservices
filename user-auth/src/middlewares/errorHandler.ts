ebfcaef77a692b57ed9cae2aeb86d7f14152a37c
Matias Fiordelli <>
2025-01-12 19:44:21 -0300

    user-auth-feat: Defining middlewares, interfaces, and a schema for Zod validation

---
+++ user-auth/src/middlewares/errorHandler.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+
+export const errorHandler = (
+	err: Error,
+	req: Request,
+	res: Response,
+	next: NextFunction
+): void => {
+    
+	if (err.name === "InvalidInputData") {
+		res.status(400).json({
+			message: err.message,
+		});
+		return;
+	}
+
+	if (err.name === "MongodbUriNotDefined") {
+		res.status(500).json({
+			message: err.message,
+		});
+		return;
+	}
+
+	res.status(500).json({ message: "Internal Server Error" });
+};
